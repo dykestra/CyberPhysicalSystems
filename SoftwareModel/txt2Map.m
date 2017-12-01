91cd372ffb69a26c19c1d7d0b5790e25b79142e1
Jukka Soikkeli <jes15@ic.ac.uk>
2017-12-01 11:33:04 +0000

    Fixed a bug in txt2map that caused crash if targets/obstacles < 2
    The function length() was used on targets and obstacles matrices
    for returning the number of targets/obstacles, given by the number
    of rows). The length() function gives the highest dimension of a
    matrix, so it worked fine when there were at least two targets and
    obstacles, but would fail with 1 osbtacle/target, as the matrices
    in question always have 2 columns (x and y).
    The function was replaced with size(M,1), which gives the
    size of the 1st dimension of the matrix M, so the number of rows.

--- SoftwareModel/txt2Map.m
+++ SoftwareModel/txt2Map.m
@@ -7,13 +7,13 @@ targets = M(M(:,3) == 1,1:2);    % Third column = +1 means it's a target
 MAP=2*ones(50,50);
 MAP(1,1)=1;
 
- for i=1:1:length(obstacles)
+ for i=1:1:size(obstacles,1)
      aux1=obstacles(i,1)+1;
      aux2=obstacles(i,2)+1;
      MAP(aux1,aux2)=-1;
  end;
 
-for j=1:1:length(targets)
+for j=1:1:size(targets,1)
     aux3=targets(j,1)+1;
     aux4=targets(j,2)+1;
     MAP(aux3,aux4)=0;
