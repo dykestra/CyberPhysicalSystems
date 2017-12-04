4c52a1cbe00c8b20d1c1cf0492f94df6bd221925
Jukka Soikkeli <jes15@ic.ac.uk>
2017-12-04 14:31:22 +0000

    Added code for analysing path planning time via simulations

---
+++ SoftwareModel/PathPlanningTimeAnalysis.m
@@ -0,0 +1,17 @@
+function RunPathPlanningTimeAnalysis(InputFile,OutputFile)
+    %% Load Variables and perform Path Planning
+    tic;
+
+    run('setup.m');
+    MAP = txt2Map(InputFile);
+    [targets,waypoints] = path_planning(MAP);
+
+    ElapsedTimePP = toc;
+
+    display(InputFile)
+    display(ElapsedTimePP)
+    
+    %% Save time information on csv
+    %line = [InputFile string(ElapsedTimePP)];
+    dlmwrite(OutputFile,ElapsedTimePP,'delimiter',',','-append');
+end
