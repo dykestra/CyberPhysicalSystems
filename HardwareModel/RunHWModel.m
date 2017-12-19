96284bd6ad170aa31e2455303a4b223fc30db767
Jukka Soikkeli <jes15@ic.ac.uk>
2017-12-19 12:16:47 +0000

    Added reporting and additional PID parameters to RunHWModel.m

--- HardwareModel/RunHWModel.m
+++ HardwareModel/RunHWModel.m
@@ -5,45 +5,68 @@ warning('off','Simulink:Engine:OutputNotConnected');
 close all
 clear all
 
-InputFile = 'MontecarloScenarios/Input/scenario4.csv';
-OutputPath = 'SingleScenarios/Output';
+InputFile = 'MontecarloScenarios/Input/scenario1.csv';
+%OutputPath = 'SingleScenarios/Output';
+filename = dir('MontecarloScenarios/Input/scenario1.csv');
+scen_name = strsplit(filename.name,'.');
+OutputPath = strcat('MontecarloScenarios/Output/2/',scen_name(1));
 
 %% Function done by other group required for scoring
 [obstacles_csv, targets_csv] = getCourseFromCSV(InputFile);
 
 %% Load Variables and perform Path Planning
 tic;
-run('setup.m');
+setup;
+%run('setup.m');
 MAP = txt2Map(InputFile);
 [waypoints] = path_planning(MAP);
 ElapsedTime = toc
 
 %% Define PIDs parameters for model
-PC1x = 2.0;
-IC1x = 0.1;
-DC1x = 0.3;
+PC1x = 1.2;
+IC1x = 0.08;
+DC1x = 0.2;
 
-PC2x = 3.5;
+PC2x = 2.0;
 IC2x = 0.3;
 DC2x = 0.3;
 
-PC1y = 2.0;
-IC1y = 0.1;
-DC1y = 0.3;
+PC3x = 2.5;
+IC3x = 0.4;
+DC3x = 0.4;
 
-PC2y = 3.5;
+PC4x = 2.7;
+IC4x = 0.6;
+DC4x = 0.5;
+
+PC1y = 1.2;
+IC1y = 0.08;
+DC1y = 0.2;
+
+PC2y = 2.0;
 IC2y = 0.3;
 DC2y = 0.3;
 
+PC3y = 2.5;
+IC3y = 0.4;
+DC3y = 0.4;
+
+PC4y = 2.7;
+IC4y = 0.6;
+DC4y = 0.5;
+
 %% Run Model
 Crane3D_DevDriv;
-set_param('Crane3D_DevDriv','StopTime','120');
+stop_time='60';
+set_param('Crane3D_DevDriv','StopTime',stop_time);
+%set_param('Crane3D_DevDriv','StopTime','40');
 set_param('Crane3D_DevDriv/Constant','Value','waypoints');
 set_param('Crane3D_DevDriv','SimulationCommand','update');
 set_param('Crane3D_DevDriv','SimulationCommand','WriteDataLogs');
 set_param('Crane3D_DevDriv','SimulationCommand','start');
 
-pause(130)
+pause(str2num(stop_time)+10)
+%pause(130)
 
 %% Score run & Post simulation analysis
 simout.Time = xpos.time;
@@ -57,7 +80,7 @@ simout.DesX = [XDes.time XDes.signals.values];
 simout.DesY = [XDes.time YDes.signals.values];
 score = getScore(simout, obstacles_csv, targets_csv, ElapsedTime)
 
-pause(.25)
+%pause(.25)
 
 % TrajX = simout.get('TrajX');
 % TrajY = simout.get('TrajY');
@@ -73,7 +96,9 @@ pause(.25)
 PostSimAnalysis(OutputPath, MAP, waypoints, simout.TrajX, simout.TrajY, simout.CartX, simout.CartY, simout.AngleX, simout.AngleY, simout.DesX, simout.DesY);
 % 
 % %% Save all information as csv and print figures
-% csvwrite('Results.csv', [TrajX, TrajY(:,2), CartX(:,2), CartY(:,2), CartdX(:,2), CartdY(:,2), AngleX(:,2), AngleY(:,2), AngledX(:,2), AngledY(:,2)]);
+output_data = strcat(OutputPath,'/Results.csv');
+csvwrite(char(output_data), [simout.TrajX, simout.TrajY(:,2), simout.CartX(:,2), simout.CartY(:,2), simout.AngleX(:,2), simout.AngleY(:,2), simout.DesX(:,2), simout.DesY(:,2)]);
+%csvwrite('Results.csv', [TrajX, TrajY(:,2), CartX(:,2), CartY(:,2), CartdX(:,2), CartdY(:,2), AngleX(:,2), AngleY(:,2), AngledX(:,2), AngledY(:,2)]);
 % 
 % % Switch warnings relating to inputs and outputs not connected back on
 % warning('on','Simulink:Engine:InputNotConnected');
